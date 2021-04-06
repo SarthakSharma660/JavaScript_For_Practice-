c9577207d04eaa11c88892a7c226a54f02965d2a
sarthaksharma660 <sarthaksharma600@gmail.com>
2021-04-06 22:25:37 +0530

    Practice Rectangle

---
+++ Rectangle_Clicked/event.js
@@ -0,0 +1,6 @@
+function doSomething(){
+    console.log("Rectangle Clicked");
+    document.getElementById("block").style.background="crimson"
+    document.getElementById("block").style.width="500px"
+    document.getElementById("block").style.borderRadius="100%"
+}
