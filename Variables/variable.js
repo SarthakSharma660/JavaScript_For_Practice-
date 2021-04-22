b93f1330a4cd138582ab1a1b539b96b9552900f3
sarthaksharma660 <sarthaksharma600@gmail.com>
2021-04-22 21:47:25 +0530

    output formate

---
+++ Variables/variable.js
@@ -0,0 +1,23 @@
+const uid="abcd123";
+var firstName ="Sarthak";
+var lastName="Sharma";
+var email="Sarthak_javascript@practice.com";
+var password="abc1234";
+var confirmPassword="abc1234";
+var state ="Delhi";
+var country = "India";
+var isLoggedInFromGoogle= false;
+var isLoggedInFromFacebook= false;  
+var courseCount = 0;
+// Console log type 1
+console.log(`
+    Firse Name :${firstName}
+    Last Name  :${lastName}
+    Email address :${email}
+    State :${state}
+    Country :${country}
+    Course Count :${courseCount}
+`);
+// Console log type 2
+console.log("Firse Name :" +firstName);
+console.log("Last Name  :" + lastName);
